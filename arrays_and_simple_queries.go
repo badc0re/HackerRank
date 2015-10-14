d5c1c0ad368925589a1bf9d3331e5c1636ab51d8
Dame Jovanoski <dame.jovanoski@gmail.com>
2015-10-14 23:18:55 +0200

    MOdified

--- arrays_and_simple_queries.go
+++ arrays_and_simple_queries.go
@@ -5,8 +5,9 @@ import (
 )
 
 type node struct {
-	next  *node
-	value int
+	next       *node
+	value      int
+	start_node bool
 }
 type LinkedList struct {
 	head   *node
@@ -15,61 +16,68 @@ type LinkedList struct {
 }
 
 func (ll *LinkedList) add_element(element int) {
-	new_node := &node{next: nil, value: element}
+
+	empty_node := &node{next: nil, start_node: true}
 	if ll.head == nil {
-		ll.head = new_node
+		ll.head = empty_node
 		ll.tail = ll.head
-	} else {
-		ll.tail.next = new_node
-		ll.tail = ll.tail.next
 	}
+
+	new_node := &node{next: nil, value: element}
+	ll.tail.next = new_node
+	ll.tail = ll.tail.next
 	fmt.Println(ll)
 	ll.lenght++
 }
 
-func (ll *LinkedList) move_elements_front(start_range int, end_range int) {
-	if start_range > ll.lenght || end_range > ll.lenght {
-		return
-	}
-	var start_range_count int = 0
-	for true {
-		start_range_count++
-		if start_range_count == start_range {
-			break
-		}
-	}
-}
-
-func (ll *LinkedList) move_elements_back(start_range int, end_range int) {
-	if start_range > ll.lenght || end_range > ll.lenght {
-		return
-	}
-	var start_range_count int = 0
-	for true {
-		start_range_count++
-		if start_range_count == start_range {
-			break
-		}
-	}
-
-}
-
-func define_query(ll LinkedList, query int, start_range int, end_range int) {
-	if query == 1 {
-		ll.move_elements_front(start_range, end_range)
-	} else if query == 2 {
-		ll.move_elements_back(start_range, end_range)
-	}
-}
-
 func (ll *LinkedList) print_ll() {
-	current := *ll.head
+	current := ll.head.next
 	for true {
 		fmt.Print(current.value, " ")
 		if current.next == nil {
 			break
 		}
-		current = *current.next
+		current = current.next
+	}
+}
+
+func (ll *LinkedList) move_elements(start_range int, end_range int, query int) {
+	// range 1 to n special case
+	if start_range > ll.lenght || end_range > ll.lenght {
+		return
+	}
+
+	start_pnt := ll.head
+	start_count := 0
+	for ; start_count < start_range-1; start_count++ {
+		fmt.Println("start_range:", start_pnt)
+		start_pnt = start_pnt.next
+	}
+
+	end_pnt := start_pnt
+	for ; start_count < end_range; start_count++ {
+		fmt.Println("end_range:", start_count, end_range)
+		end_pnt = end_pnt.next
+	}
+
+	if query == 2 {
+		ll.tail.next = start_pnt.next
+		start_pnt.next = end_pnt.next
+		end_pnt.next = nil
+	} else if query == 1 {
+
+	}
+
+	fmt.Println(ll.head)
+}
+
+func define_query(ll LinkedList, query int, start_range int, end_range int) {
+	if query == 1 {
+		//move_elements_front
+		ll.move_elements(start_range, end_range, query)
+	} else if query == 2 {
+		//move_elements_back
+		ll.move_elements(start_range, end_range, query)
 	}
 }
 
@@ -80,6 +88,15 @@ func main() {
 	ll.add_element(3)
 	ll.add_element(4)
 	ll.add_element(5)
+	ll.add_element(6)
+	ll.add_element(7)
+	ll.add_element(8)
+	fmt.Print("Before: ")
+	ll.print_ll()
+	fmt.Println()
+	define_query(ll, 2, 1, 4)
+
+	fmt.Print("Aflter: ")
 	ll.print_ll()
 	// scanner := bufio.NewScanner(os.Stdin)
 	// for scanner.Scan() {
